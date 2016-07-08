e6d9869fae69b5e693a6015c3700929342476d62
Jason Juhyun Kim <mtinet@hanmail.net>
2016-07-08 09:40:55 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,40 @@
 var video;
+var slider;
 
 function setup() {
-  createCanvas(320, 240);
-  background(51);
+  canvas = createCanvas(640, 480, WEBGL);
+  canvas.id('p5canvas');
   video = createCapture(VIDEO);
-  video.size(320, 240);
-  // video.hide();
+  video.size(640, 480);
+  video.id('p5video');
+  //video.hide();
+  //slider = createSlider(0, 1, 0.5, 0.01);
+  //slider.id('blur-slider');
 
-}
+  var seriously = new Seriously();
 
-function draw() {
-  tint(255, 0, 150);
-  image(video, 0, 0, mouseX, height);
+  var src = seriously.source('#p5video');
+  var target = seriously.target('#p5canvas');
+
+  // var blur = seriously.effect('blur');
+  // blur.amount = '#blur-slider';
+  // blur.source = src;
+  // target.source = blur;
+
+  var chroma = seriously.effect('chroma');
+  chroma.source = src;
+  target.source = chroma;
+  var r = 98 / 255;
+  var g = 175 / 255;
+  var b = 116 / 255;
+  chroma.screen = [r,g,b,1];
+
+
+
+
+
+  seriously.go();
+
+
+
 }
