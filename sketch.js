8bf0f6b8fa6dce55bde0418afc0db834f2047afb
Jason Juhyun Kim <mtinet@hanmail.net>
2016-07-08 09:57:14 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,22 @@
 var video;
-var slider;
+var vScale = 16;
+
+var particles = [];
 
 function setup() {
-  canvas = createCanvas(640, 480, WEBGL);
-  canvas.id('p5canvas');
+  createCanvas(640,480);
+  pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(640, 480);
-  video.id('p5video');
- // video.hide();
-  slider = createSlider(0, 1, 0.5, 0.01);
-  slider.id('blur-slider');
-
-  var seriously = new Seriously();
-
-  var src = seriously.source('#p5video');
-  var target = seriously.target('#p5canvas');
-
-  var blur = seriously.effect('blur');
-  blur.amount = '#blur-slider';
-  blur.source = src;
-  target.source = blur;
-
-  var chroma = seriously.effect('chroma');
-  chroma.source = src;
-  target.source = chroma;
-  var r = 98 / 255;
-  var g = 175 / 255;
-  var b = 116 / 255;
-  chroma.screen = [r,g,b,1];
-
-
-
-
-
-  seriously.go();
-
-
-
+  video.size(width/vScale, height/vScale);
+  for (var i = 0; i < 100; i++) {
+    particles[i] = new Particle(320,240);
+  }
+  background(51);
 }
+
+function draw() { 
+  video.loadPixel();
+  for(var i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particle[i].show();
+  }
