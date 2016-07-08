5ad3f924e5e013a3766e25fb86aacb01f9650ccc
Jason Juhyun Kim <mtinet@hanmail.net>
2016-07-08 10:16:09 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,15 @@
 var video;
-var vScale = 16;
-
-var particles = [];
 
 function setup() {
-  createCanvas(640,480);
-  pixelDensity(1);
-  video = createCapture(VIDEO);
-  video.size(width/vScale, height/vScale);
-  for (var i = 0; i < 100; i++) {
-    particles[i] = new Particle(320,240);
-  }
+  createCanvas(320, 240);
   background(51);
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  // video.hide();
+
 }
 
-function draw() { 
-  video.loadPixel();
-  for(var i = 0; i < particles.length; i++) {
-    particles[i].update();
-    particle[i].show();
-  }
+function draw() {
+  tint(255, 0, 150);
+  image(video, 0, 0, mouseX, height);
 }
