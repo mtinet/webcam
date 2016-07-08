0735a7c21562cdb9da4cb724c5910bd2f768df56
Jason Juhyun Kim <mtinet@hanmail.net>
2016-07-08 10:44:36 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 var video;
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 480);
   background(51);
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(800, 480);
   video.hide();
 
 }
