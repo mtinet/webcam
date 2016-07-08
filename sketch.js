2715d14ef01eaf08f949698e838096280d5c8fd7
Jason Juhyun Kim <mtinet@hanmail.net>
2016-07-08 09:39:08 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,29 @@
 var video;
 
-function setup() {
-  createCanvas(640, 480);
-  background(51);
-  video = createCapture(VIDEO);
-  video.size(640, 480);
-  // video.hide();
+var x = 0;
 
+function setup() {
+  createCanvas(800, 240);
+  pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  background(51);
 }
 
 function draw() {
-  tint(50, 100, 150);
-  image(video, 0, 0, mouseX, height);
+  video.loadPixels();
+  // image(video, 0, 0);
+
+  var w = video.width;
+  var h = video.height;
+
+  copy(video, w/2, 0, 1, h, x, 0, 1, h);
+
+  x = x + 1;
+  
+  if (x > width) {
+    x = 0;
+  }
+
+
 }
